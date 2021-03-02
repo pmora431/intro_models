20ab582bf8c129c86e2c1fe010b96efaf7cfa6fe
pmora431 <pmora431@hotmail.es>
2021-03-02 10:11:33 -0600

    Third Commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,20 @@
 class UsersController < ApplicationController
 
     def form
+        @user = User.new
     end
 
     def create
         @user = User.create(name: params["name"], email: params["email"])
+        if @user.save
+            redirect_to '/users/index'
+        else
+            render 'form'
+        end
+    end
+
+    def index
+        @users = User.all
     end
 
 end
