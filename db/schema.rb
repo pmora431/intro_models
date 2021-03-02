d11c282b8f6ef2355923f1269f4e4e9bcf4d7a05
pmora431 <pmora431@hotmail.es>
2021-03-02 17:01:53 -0600

    Fourth commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_02_142522) do
+ActiveRecord::Schema.define(version: 2021_03_02_225232) do
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "zombies", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
